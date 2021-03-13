e51ddbf9ac8f8d45006aaa8cdd3eda1fe5df6573
Hawk Song <savokisong@gmail.com>
2021-03-12 23:42:16 -0800

    parse url and fetch more data

--- src/order/App.jsx
+++ src/order/App.jsx
@@ -1,11 +1,27 @@
+import React, { useCallback, useEffect, useMemo } from 'react';
+import URI from 'urijs';
+import dayjs from 'dayjs';
 import './App.css'
 import Header from '../common/Header.jsx';
+import Detail from '../common/Detail.jsx';
 import Account from './Account.jsx';
 import Choose from './Choose.jsx';
 import Passengers from './Passengers.jsx';
 import Ticket from './Ticket.jsx';
 
+
 import {connect} from 'react-redux'
+
+import {
+    setDepartStation,
+    setArriveStation,
+    setTrainNumber,
+    setSeatType,
+    setDepartDate,
+    setSearchParsed,
+
+    fetchInitial,
+} from './actions';
 
 function App(props) {
     const {
@@ -26,8 +42,64 @@ function App(props) {
         dispatch,
     } = props;
 
+    const onBack = useCallback(() => {
+        window.history.back();
+    }, []);
+
+    useEffect(() => {
+        const queries = URI.parseQuery(window.location.search);
+
+        const { trainNumber, dStation, aStation, type, date } = queries;
+
+        dispatch(setDepartStation(dStation));
+        dispatch(setArriveStation(aStation));
+        dispatch(setTrainNumber(trainNumber));
+        dispatch(setSeatType(type));
+        dispatch(setDepartDate(dayjs(date).valueOf()));
+        dispatch(setSearchParsed(true));
+    }, []);
+
+    useEffect(() => {
+        if (!searchParsed) {
+            return;
+        }
+
+        const url = new URI('/rest/order')
+            .setSearch('dStation', departStation)
+            .setSearch('aStation', arriveStation)
+            .setSearch('type', seatType)
+            .setSearch('date', dayjs(departDate).format('YYYY-MM-DD'))
+            .toString();
+        dispatch(fetchInitial(url));
+    }, [searchParsed, departStation, arriveStation, seatType, departDate]);
+
+    if (!searchParsed) {
+        return null;
+    }
+
     return(
-        <div className="app"></div>
+        <div className="app">
+            <div className="header-wrapper">
+                <Header title="Fill Order" onBack={onBack}/>
+            </div>
+            <div className="detail-wrapper">
+                <Detail
+                    departDate={departDate}
+                    arriveDate={arriveDate}
+                    departTimeStr={departTimeStr}
+                    arriveTimeStr={arriveTimeStr}
+                    trainNumber={trainNumber}
+                    departStation={departStation}
+                    arriveStation={arriveStation}
+                    durationStr={durationStr}
+                >
+                    <span
+                        style={{ display: 'block' }}
+                        className="train-icon"
+                    ></span>
+                </Detail>
+            </div>
+        </div>
     )
 }
 
