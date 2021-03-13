77ad3d787da7728fccbca13da45b74ccc0ff6bf7
Hawk Song <savokisong@gmail.com>
2021-03-12 16:50:55 -0800

    parse url  get more data

--- src/ticket/App.jsx
+++ src/ticket/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useEffect, useCallback} from 'react';
+import Header from '../common/Header.jsx';
+import useNav from '../common/useNav';
+import Nav from '../common/Nav.jsx';
+import URI from 'urijs';
+import dayjs from 'dayjs';
+import { h0 } from '../common/fp';
 import './App.css'
 import Detail from '../common/Detail.jsx';
 import Candidate from './Candidate.jsx';
 
 import {connect} from 'react-redux'
+
+import {
+    setDepartStation,
+    setArriveStation,
+    setTrainNumber,
+    setDepartDate,
+    setSearchParsed,
+    prevDate,
+    nextDate,
+
+    setDepartTimeStr,
+    setArriveTimeStr,
+    setArriveDate,
+    setDurationStr,
+    setTickets,
+} from './actions';
+
 
 function App(props) {
     const {
@@ -22,8 +45,82 @@ function App(props) {
         dispatch,
     } = props;
 
+    const onBack = useCallback(() => {
+        window.history.back();
+    }, []);
+
+    useEffect(() => {
+        const queries = URI.parseQuery(window.location.search);
+        const { aStation, dStation, date, trainNumber } = queries;
+
+        dispatch(setDepartStation(dStation));
+        dispatch(setArriveStation(aStation));
+        dispatch(setTrainNumber(trainNumber));
+        dispatch(setDepartDate(h0(dayjs(date).valueOf())));
+
+        dispatch(setSearchParsed(true));
+    }, []);
+
+    useEffect(() => {
+        document.title = trainNumber;
+    }, [trainNumber]);
+
+    useEffect(() => {
+        if (!searchParsed) {
+            return;
+        }
+
+        const url = new URI('/rest/ticket')
+            .setSearch('date', dayjs(departDate).format('YYYY-MM-DD'))
+            .setSearch('trainNumber', trainNumber)
+            .toString();
+
+        fetch(url)
+            .then(response => response.json())
+            .then(result => {
+                const { detail, candidates } = result;
+
+                const {
+                    departTimeStr,
+                    arriveTimeStr,
+                    arriveDate,
+                    durationStr,
+                } = detail;
+
+                dispatch(setDepartTimeStr(departTimeStr));
+                dispatch(setArriveTimeStr(arriveTimeStr));
+                dispatch(setArriveDate(arriveDate));
+                dispatch(setDurationStr(durationStr));
+                dispatch(setTickets(candidates));
+            });
+    }, [searchParsed, departDate, trainNumber]);
+
+    const { isPrevDisabled, isNextDisabled, prev, next } = useNav(
+        departDate,
+        dispatch,
+        prevDate,
+        nextDate
+    );
+
+    if (!searchParsed) {
+        return null;
+    }
+
     return (
-        <div>"tick page"</div>
+        <div className="app">
+            <div className="header-wrapper">
+                <Header title={trainNumber} onBack={onBack} />
+            </div>
+            <div className="nav-wrapper">
+                <Nav
+                    date={departDate}
+                    isPrevDisabled={isPrevDisabled}
+                    isNextDisabled={isNextDisabled}
+                    prev={prev}
+                    next={next}
+                />
+            </div>
+        </div>
     )
 
 }
